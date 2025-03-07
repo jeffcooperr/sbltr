9bfc124b5aac99ed2f63672906134bd333291284
benAkeane <benthelen88@gmail.com>
2025-03-07 14:17:11 -0500

    update files

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ FIREBASE_WEB_API_KEY = os.getenv('FIREBASE_WEB_API_KEY')
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 # Initialize Firestore
-cred = credentials.Certificate('path')  # Update with the correct path
+cred = credentials.Certificate('sbltr-c125d-firebase-adminsdk-fbsvc-384b0b17a1.json')  # Update with the correct path
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
